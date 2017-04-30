719a0114db84cbaf5781a38056aee765286f1ae2
prantoran <prantoran@gmail.com>
2017-05-01 01:22:45 +0600

    Flappy Bird sdl prac

---
+++ flappy_gopher/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/veandco/go-sdl2/sdl"
+	ttf "github.com/veandco/go-sdl2/sdl_ttf"
+)
+
+func main() {
+
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(2)
+	}
+}
+
+func run() error {
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		return fmt.Errorf("could not initialize SDL: %v", err)
+	}
+	defer sdl.Quit()
+
+	if err = ttf.Init(); err != nil {
+		return fmt.Errorf("Could not initialize ttf: %v", err)
+	}
+	defer ttf.Quit()
+
+	//w = window, r = renderer
+	w, r, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("could not create window: %v", err)
+	}
+	defer w.Destroy() //shall destroy window and renderer
+
+	_ = r
+
+	time.Sleep(5 * time.Second)
+
+	return drawTitle()
+
+}
+
+func drawTitle() error {
+	f, err := ttf.OpenFont("res/fonts/flappy.ttf", 20)
+	if err != nil {
+		return fmt.Errorf("could not load font: %v", err)
+	}
+	c := sdl.Color{R: 255, G: 100, B: 0, A: 255}
+	f.RenderUTF8_Solid("flappy gopher", c)
+	_ = f
+	return nil
+}
