ba3aa283f99b69800b8bac0ed4c84b9bc2a97d01
pinku <prantoran@gmail.com>
2017-07-02 15:58:37 +0600

    Random

--- spreadsheet_Iwark/main.go
+++ spreadsheet_Iwark/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 
@@ -10,7 +11,7 @@ import (
 )
 
 const (
-	SpreadSheetID = "1mYiA2T4_QTFUkAXk0BE3u7snN2o5FgSRqxmRrn_Dzh4"
+	SpreadSheetID = "1N4DJ8RiUV00E3EwZP4q_A7qR4F9BD5mhtapLJn7s6gI"
 )
 
 func main() {
@@ -18,23 +19,33 @@ func main() {
 	checkError(err)
 	conf, err := google.JWTConfigFromJSON(data, spreadsheet.Scope)
 	checkError(err)
+	fmt.Printf("conf type: %T \n\n", conf)
+	fmt.Printf("email: %v\nexpires: %v\nprivatekey: %v\nprivatekeyid: %v\n", conf.Email, conf.Expires, conf.PrivateKey, conf.PrivateKeyID)
+	fmt.Printf("scope: %v\nsubject: %v\n,tokenurl: %v\n", conf.Scopes, conf.Subject, conf.TokenURL)
+
+	n := bytes.Index(conf.PrivateKey, []byte{0})
+	fmt.Printf("n: %v\n", n)
+	s := string(conf.PrivateKey[:1690])
+	fmt.Printf("s: %v \n", s)
+
 	client := conf.Client(context.TODO())
 
 	service := spreadsheet.NewServiceWithClient(client)
-	spreadsheet, err := service.FetchSpreadsheet("1mYiA2T4_QTFUkAXk0BE3u7snN2o5FgSRqxmRrn_Dzh4")
+	spreadsheet, err := service.FetchSpreadsheet(SpreadSheetID)
 	checkError(err)
 	sheet, err := spreadsheet.SheetByIndex(0)
 	checkError(err)
 	for _, row := range sheet.Rows {
 		for _, cell := range row {
-			fmt.Println(cell.Value)
+			fmt.Printf(cell.Value)
 		}
+		fmt.Printf("\n")
 	}
 
-	// Update cell content
-	sheet.Update(0, 0, "hogehoge")
+	//Update cell content
+	sheet.Update(0, 4, "sakura")
 
-	// Make sure call Synchronize to reflect the changes
+	//Make sure call Synchronize to reflect the changes
 	err = sheet.Synchronize()
 	checkError(err)
 }
